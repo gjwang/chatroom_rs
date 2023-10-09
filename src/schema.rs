9346bfdadaed0519346247c020105e25ef20022c
0xMaster <guijiewan@gmail.com>
2023-10-10 00:54:40 +0800

    fixed schema

--- src/schema.rs
+++ src/schema.rs
@@ -1,2 +1,9 @@
 // @generated automatically by Diesel CLI.
 
+diesel::table! {
+    persons (id) {
+        id -> Nullable<Integer>,
+        name -> Text,
+        age -> Nullable<Integer>,
+    }
+}
